2ff5f3bf0a0618742af5c8e4f784d46b3157e886
axejah <57503002+axejah@users.noreply.github.com>
2020-09-08 21:28:36 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Scheduled SMS
+Simple SMS cron-job that sends happy 1337 at 13:37 using Twilio API service.
---
+++ server.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const accountSid = process.env.ACC_SID;
+const authToken = process.env.AUTH_TOKEN;
+
+const client = require("twilio")(accountSid, authToken);
+const express = require("express");
+const cron = require("node-cron");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+function sendSms(num) {
+  client.messages
+    .create({
+      body: "HAPPY 1337!!",
+      from: "LEET",
+      to: num,
+    })
+    .then((message) => console.log(message.sid))
+    .catch((err) => console.log(err));
+}
+
+cron.schedule("37 13 * * *", () => {
+  sendSms("+31629444605");
+  sendSms("+31648228243");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on PORT: ${port}`);
+});
